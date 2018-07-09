1f5707a32d1b3ea294f9499910ba8fa71ba3a6de
rmrhz <rmrhz@tfwno.gf>
2018-07-10 02:07:06 +0900

    Add DB init in Platform constructor

--- src/Platform.php
+++ src/Platform.php
@@ -4,5 +4,17 @@ namespace News;
 
 class Platform
 {
+	/**
+	 * PDO Instance
+	 * https://packagist.org/packages/lincanbin/php-pdo-mysql-class
+	 *
+	 * @var \DB
+	 */
+	protected $db;
+
+	public function __construct()
+	{
+		$this->db = new DB(env('DB_HOST', 'localhost'), env('DB_NAME', 'duhnews'), env('DB_USER', 'root'), env('DB_PASS', ''));
+	}
 	
 }
