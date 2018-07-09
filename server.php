84564a238b69f13ee36af227e447ec582a36c112
rmrhz <rmrhz@tfwno.gf>
2018-07-10 02:45:16 +0900

    Add routing

--- server.php
+++ server.php
@@ -10,3 +10,13 @@ $dotenv->load();
 
 // Routing Layer
 $router = new RouteCollector();
+
+$router->get('/', ['News\Platform', 'getIndex']);
+$router->get('/create' ['News\Platform', 'getAddBulletin']);
+$router->post('/create', ['News\Platform', 'postAddBulletin']);
+$router->get('/{bulletin_id}/remove', ['News\Platform', 'getRemoveBulletin']);
+$router->get('/{bulletin_id}', ['News\Platform', 'getViewBulletin']);
+$router->get('/{bulletin_id}/comments', ['News\Platform', 'getBulletinComments']);
+$router->get('/{bulletin_id}/comments/create', ['News\Platform', 'getAddBulletinComment']);
+$router->post('/{bulletin_id}/comments/create', ['News\Platform', 'getAddBulletinComment']);
+$router->get('/{bulletin_id}/comments/{bulletin_comment_id}/remove', ['News\Platform', 'getRemoveBulletinComment']);
