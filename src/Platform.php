ae6454c24c89721ab901116b1fbf0618539362a2
rmrhz <rmrhz@tfwno.gf>
2018-07-10 02:48:54 +0900

    Add skeletal functions

--- src/Platform.php
+++ src/Platform.php
@@ -18,5 +18,50 @@ class Platform
 	{
 		$this->db = new DB(env('DB_HOST', 'localhost'), env('DB_NAME', 'duhnews'), env('DB_USER', 'root'), env('DB_PASS', ''));
 	}
+
+	public function getIndex()
+	{
+
+	}
+
+	public function getAddBulletin()
+	{
+
+	}
+
+	public function postAddBulletin()
+	{
+
+	}
+
+	public function getRemoveBulletin()
+	{
+
+	}
+
+	public function getViewBulletin()
+	{
+
+	}
+
+	public function getBulletinComments()
+	{
+
+	}
+
+	public function getAddBulletinComment()
+	{
+		
+	}
+
+	public function postAddBulletinComment()
+	{
+
+	}
+
+	public function getRemoveBulletinComment()
+	{
+
+	}
 	
 }
