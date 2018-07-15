3cfcee4f0dba7dc45e97ff25e7be29186df3da09
rmrhz <rmrhz@tfwno.gf>
2018-07-16 00:09:04 +0900

    Add Core\Routing\HandlerResolver class

---
+++ src/Core/Routing/HandlerResolver.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace News\Core\Routing;
+
+class HandlerResolver implements \Phroute\Phroute\HandlerResolverInterface {
+
+	public function __construct(ContainerBuilder $container)
+	{
+		$this->container = $container;
+	}
+	
+	/**
+	 * Create an instance of the given handler.
+	 *
+	 * @param $handler
+	 * @return array
+	 */
+	
+	public function resolve ($handler)
+	{
+		if(is_array($handler) && is_string($handler[0]))
+		{
+			$handler[0] = new $handler[0];
+		}
+		
+		return $handler;
+	}
+}
