918180c71c05833611a9426f6071d25dcb7dfb5a
rmrhz <rmrhz@tfwno.gf>
2018-08-01 16:23:18 +0900

    Move routing from server.php to routes/base.php

--- server.php
+++ server.php
@@ -26,13 +26,3 @@ $container->compile(true);
 foreach(glob(ROOT . '/routes/*.php') as $route) {
     require_once $route;
 }
-
-$router->get('/', ['News\Platform', 'getIndex']);
-$router->get('/create', ['News\Platform', 'getAddBulletin']);
-$router->post('/create', ['News\Platform', 'postAddBulletin']);
-$router->get('/{bulletin_id}/remove', ['News\Platform', 'getRemoveBulletin']);
-$router->get('/{bulletin_id}', ['News\Platform', 'getViewBulletin']);
-$router->get('/{bulletin_id}/comments', ['News\Platform', 'getBulletinComments']);
-$router->get('/{bulletin_id}/comments/create', ['News\Platform', 'getAddBulletinComment']);
-$router->post('/{bulletin_id}/comments/create', ['News\Platform', 'postAddBulletinComment']);
-$router->get('/{bulletin_id}/comments/{bulletin_comment_id}/remove', ['News\Platform', 'getRemoveBulletinComment']);
