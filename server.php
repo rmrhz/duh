1a1d6fd275b005509a43a23e0a706f6221939ddf
rmrhz <rmrhz@tfwno.gf>
2019-09-09 17:22:19 +0900

    Move services.yaml to config directory and rewrite server.php

--- server.php
+++ server.php
@@ -13,12 +13,14 @@ define('ROOT', dirname(__FILE__));
 $dotenv = new Dotenv;
 $dotenv->load(ROOT . '/.env');
 
+$locator = new FileLocator(ROOT . '/config');
+
 // Routing Layer
 $router = new RouteCollector();
 
 $container = new ContainerBuilder();
 $container->setParameter('container', $container);
-$loader = new YamlFileLoader($container, new FileLocator(ROOT));
+$loader = new YamlFileLoader($container, $locator);
 $loader->load('services.yaml');
 
 $container->compile(true);
