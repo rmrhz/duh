5eaa9e61fe0dc8c407ec5c43851f2ce5f8f26ffd
rmrhz <rmrhz@tfwno.gf>
2019-09-11 19:29:32 +0900

    Update Http\Controller for Request to be injected in setter

--- src/Http/Controller.php
+++ src/Http/Controller.php
@@ -2,10 +2,18 @@
 
 namespace News\Core\Http;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
 abstract class Controller
 {
+    /**
+     * Symfony's HttpFoundation's Request container
+     *
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     */
+    protected $request;
+
     /**
      * Returns the response in JSON format
      *
@@ -14,5 +22,15 @@ abstract class Controller
     public function jsonResponse($data) : JsonResponse
     {
         return new JsonResponse($data);
+    }
+
+    /**
+     * Sets the Request
+     *
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     */
+    public function setRequest(Request $request)
+    {
+        $this->request = $request;
     }
 }
